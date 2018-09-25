c2a0fbecc4c4363d4c45edd75619e04d70dffaab
hitler11319 <hitler11319@gmail.com>
2018-09-25 21:35:26 +0800

    從server上讓使用者下載檔案（裡面是寫在console中，但其實是要寫在Controller中）

---
+++ c#/FromServerDownloadFile/FromServerDownloadFile/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http;
+using System.IO;
+
+namespace FromServerDownloadFile
+{
+    class Program
+    {
+        /// <summary>
+        /// 這些是要寫在Server中的（即要放入Controller中的）
+        /// 現在只是做個整理，所以執行一定是錯誤的
+        /// HttpContext和HttpUtility和using System.Web.Http之錯誤
+        /// 全屬正常現象
+        /// 如要用時，再複製使用即可
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+
+            //要傳回文字型式
+            response.Content = new StringContent("文字、或者html標籤……等相關文字組成");
+
+            //傳回檔案型式
+            string path = HttpContext.Current.Server.MapPath(@"~/subQuestion.zip");     //zip路徑
+            StreamReader sr = new StreamReader(path);
+            response.Content = new StreamContent(sr.BaseStream);
+
+            //固定寫法
+            response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment");
+
+            response.Content.Headers.ContentDisposition.FileName = HttpUtility.UrlPathEncode("檔名.txt");
+            //response.Content.Headers.ContentLength = fileStream.Length; //告知瀏覽器下載長度
+            
+        }
+    }
+}
