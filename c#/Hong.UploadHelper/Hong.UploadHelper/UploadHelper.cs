41a2a2999369ab61bb82520332d43717a473e291
hitler11319 <hitler11319@gmail.com>
2018-10-01 21:34:52 +0800

    Hong.UploadHelper(檔案上傳功能） Owner:Hong

--- c#/Hong.UploadHelper/Hong.UploadHelper/UploadHelper.cs
+++ c#/Hong.UploadHelper/Hong.UploadHelper/UploadHelper.cs
@@ -21,8 +21,9 @@ namespace Hong.UploadHelper
         /// <param name="savePath">要儲存到server的路徑（例如要放在upload底下，輸入：upload/）</param>
         /// <param name="accept">允許的副檔名格式（再做一次判斷），預設為全部皆可。例如： .zip .doc .docx</param>
         /// <param name="format">檔名格式（YYYY=今天西元年，MM=今天月，DD=今天日，EEE=今天民國年，FFFF=原上傳檔名，RRR=亂數產生編碼（為16碼英數混合，目前無法更改設定））  預設為FFFF，可自行加入編寫。例如： YYYY-MM-FFFF-RRRR0011</param>
+        /// <param name="rndCount">亂碼為幾位數，預設為16碼</param>
         /// <returns></returns>
-        public static string Upload(HttpPostedFileBase file, string savePath, string accept = "", string format = "FFFF")
+        public static string Upload(HttpPostedFileBase file, string savePath, string accept = "", string format = "FFFF", int rndCount = 16)
         {
 
             //副檔名（出來會是.zip這種格式）
@@ -37,8 +38,15 @@ namespace Hong.UploadHelper
             //如果使用者沒給/，自己加
             savePath = savePath.Substring(savePath.Length - 1) == "/" || savePath.Substring(savePath.Length - 2) == "\\" ? savePath : savePath + "/";
 
+
             //整個的儲存路徑（含server）
             string newSavePath = HttpContext.Current.Server.MapPath("~/" + savePath);
+
+            //如果沒此資料夾，新建它
+            if (!Directory.Exists(newSavePath)) { Directory.CreateDirectory(newSavePath); }
+
+            //判斷位數碼是否為1-16
+            if(rndCount > 16 || rndCount <= 0) { rndCount = 16; }
 
             //檔案的新名字（格式化）
             string newFileName = format.Replace("YYYY", DateTime.Now.Year.ToString()).
@@ -46,15 +54,18 @@ namespace Hong.UploadHelper
                                         Replace("DD", DateTime.Now.Day.ToString()).
                                         Replace("EEE", (DateTime.Now.Year - 1911).ToString()).
                                         Replace("FFFF", fileName).
-                                        Replace("RRR", bp_guid16());
+                                        Replace("RRR", bp_guid16().Substring(0, rndCount));
+
+            //如果檔名重複，自動幫他加亂數16碼在前面（因為他可能輸入FFFF，所以會有機會重複）
+            if(File.Exists(newSavePath + newFileName + extension)){newFileName = bp_guid16() + "-" + newFileName;}
 
 
             //儲存起來
-            file.SaveAs(newSavePath);
+            file.SaveAs(newSavePath + extension);
 
 
             //傳回儲存路徑 + 新檔名
-            return newSavePath + newFileName;
+            return newSavePath + newFileName + extension;
         }
 
         /// <summary>
