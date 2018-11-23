c21545bb6e5b95a61322ea7eeaa1f2edc4f25e5a
hitler11319 <hitler11319@gmail.com>
2018-11-23 21:40:44 +0800

    Hong.MalHelper（寄信相關功能）  Owner:Hong

--- c#/Hong.MailHelper/Hong.MailHelper/MailHelper.cs
+++ c#/Hong.MailHelper/Hong.MailHelper/MailHelper.cs
@@ -33,10 +33,11 @@ namespace Hong.MailHelper
         }
 
         /// <summary>
-        /// 寄信
+        /// 寄信（多個一次寄）
         /// </summary>
         /// <param name="data">Mail內容列表（List）</param>
-        public string Send(List<MailContent> data)
+        /// <param name="senderName">寄信人名字（可隨便填，預設為Hong）</param>
+        public string SendMails(List<MailContent> data, string senderName)
         {
             //成功寄出的數量
             int successCount = 0;
@@ -47,88 +48,104 @@ namespace Hong.MailHelper
             //跑過所有資料
             foreach(var d in data)
             {
-                //如果收信人一個都沒有，直接失敗且跳下一個
-                if(d.Recipient.Count() < 0) { errorCount += 1; continue; }
+                //呼叫下方的一次寄信
+                bool result = Send(d, senderName);
 
-                try
-                {
-                    //宣告MalMessage類型
-                    MailMessage msg = new MailMessage();
-
-                    //跑過所有收件人
-                    foreach (var r in d.Recipient)
-                    {
-                        //新增收件人
-                        msg.To.Add(r);
-                    }
-
-                    //跑過所有副本
-                    foreach (var c in d.Copy)
-                    {
-                        //新增副本
-                        msg.CC.Add(c);
-                    }
-
-                    //3個參數分別是發件人地址（可以隨便寫），發件人姓名，編碼
-                    msg.From = new MailAddress("XXX@gmail.com", "Hong", Encoding.UTF8);
-
-                    //標題
-                    msg.Subject = d.Title;
-
-                    //標題編碼（都用utf-8）
-                    msg.SubjectEncoding = Encoding.UTF8;
-
-                    //內文
-                    msg.Body = d.Content;
-
-                    //內文編碼（都用utf-8）
-                    msg.BodyEncoding = Encoding.UTF8;
-
-                    //跑過所有的附件
-                    foreach(var a in d.Attachment)
-                    {
-                        //新增附件
-                        msg.Attachments.Add(new Attachment(@"" + a));
-                    }
-
-                    //是否html郵件
-                    msg.IsBodyHtml = d.IsBodyHtml;
-
-                    //優先等級
-                    msg.Priority = d.Priority == 2 ? MailPriority.High : d.Priority == 1 ? MailPriority.Normal : MailPriority.Low;
-
-
-                    #region 設定完參數要開始寄信
-
-                    //宣告SmtpClient類型
-                    SmtpClient client = new SmtpClient();
-
-                    //設定寄件人的資料
-                    client.Credentials = new System.Net.NetworkCredential(SenderAddress, Password);
-
-                    //SMTP的Server
-                    client.Host = d.Host;
-
-                    client.Port = 25; //設定Port
-                    client.EnableSsl = true; //gmail預設開啟驗證
-                    client.Send(msg); //寄出信件
-                    client.Dispose();
-                    msg.Dispose();
-
-                    #endregion
-
-                    successCount += 1;
-
-                }
-                catch (Exception ex)
-                {
-                    errorCount += 1;
-                }
-
+                //更新成功或失敗
+                if (result) { successCount++; } else { errorCount++; }
             }
 
             return "成功：" + successCount.ToString() + "筆； 失敗：" + errorCount.ToString() + "筆";
         }
+
+
+        /// <summary>
+        /// 寄信（一次）
+        /// </summary>
+        /// <param name="d">Mail內容</param>
+        /// <param name="senderName">寄信人名字（可隨便填，預設為Hong）</param>
+        /// <returns></returns>
+        public bool Send(MailContent d, string senderName = "Hong")
+        {
+            //如果收信人一個都沒有，傳false
+            if (d.Recipient.Count() < 0) { return false; }
+
+            try
+            {
+                //宣告MalMessage類型
+                MailMessage msg = new MailMessage();
+
+                //跑過所有收件人
+                foreach (var r in d.Recipient)
+                {
+                    //新增收件人
+                    msg.To.Add(r);
+                }
+
+                //跑過所有副本
+                foreach (var c in d.Copy)
+                {
+                    //新增副本
+                    msg.CC.Add(c);
+                }
+
+                //3個參數分別是發件人地址（可以隨便寫），發件人姓名，編碼
+                msg.From = new MailAddress(SenderAddress, senderName, Encoding.UTF8);
+
+                //標題
+                msg.Subject = d.Title;
+
+                //標題編碼（都用utf-8）
+                msg.SubjectEncoding = Encoding.UTF8;
+
+                //內文
+                msg.Body = d.Content;
+
+                //內文編碼（都用utf-8）
+                msg.BodyEncoding = Encoding.UTF8;
+
+                //跑過所有的附件
+                foreach (var a in d.Attachment)
+                {
+                    //新增附件
+                    msg.Attachments.Add(new Attachment(@"" + a));
+                }
+
+                //是否html郵件
+                msg.IsBodyHtml = d.IsBodyHtml;
+
+                //優先等級
+                msg.Priority = d.Priority == 2 ? MailPriority.High : d.Priority == 1 ? MailPriority.Normal : MailPriority.Low;
+
+
+                #region 設定完參數要開始寄信
+
+                //宣告SmtpClient類型
+                SmtpClient client = new SmtpClient();
+
+                //設定寄件人的資料
+                client.Credentials = new System.Net.NetworkCredential(SenderAddress, Password);
+
+                //SMTP的Server
+                client.Host = d.Host;
+
+                client.Port = 25; //設定Port
+                client.EnableSsl = true; //gmail預設開啟驗證
+                client.Send(msg); //寄出信件
+                client.Dispose();
+                msg.Dispose();
+
+                #endregion
+
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
     }
 
     /// <summary>
