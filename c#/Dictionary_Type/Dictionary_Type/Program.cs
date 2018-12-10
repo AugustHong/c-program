669ee3ee7cce657491f78667da63c494ad23a922
hitler11319 <hitler11319@gmail.com>
2018-12-10 20:51:06 +0800

    字典類型 和 StringBuilder 基礎應用及介紹

--- c#/Dictionary_Type/Dictionary_Type/Program.cs
+++ c#/Dictionary_Type/Dictionary_Type/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Text;   //要使用StringBuilder時要引用
+
 namespace Dictionary_Type
 {
     class Program
@@ -39,6 +41,73 @@ namespace Dictionary_Type
                 Console.WriteLine($"key = {item.Key} , value = {item.Value}");
             }
 
+            Console.WriteLine();
+            //----------------------------------------------------------------------------------------
+
+            //StringBuilder的使用（類似字串）
+
+            //宣告方法（三種）
+            StringBuilder stringBuild1 = new StringBuilder();
+
+            //一開始給值
+            StringBuilder stringBuild2 = new StringBuilder("hello");
+
+            //給值，並且給他長度上限
+            StringBuilder stringBuild3 = new StringBuilder("hello", 20);
+
+            //設定長度上限
+            stringBuild2.Capacity = 15;
+
+            //新增值加入進去
+            stringBuild1.Append("hahaha");
+            Console.WriteLine(stringBuild1);   //輸出：hahaha
+
+            stringBuild2.Append(" world");
+            Console.WriteLine(stringBuild2);   //輸出：hello world
+
+            //新增值加入進去（並且新增的是格式化的值）
+            stringBuild3.AppendFormat(" total is {0:c} dollors", 25);   
+            Console.WriteLine(stringBuild3);   //輸出：hello total is $25.00  dollors
+
+            //將值放在某個位置中
+            stringBuild2.Insert(6, "Beautiful ");  
+            Console.WriteLine(stringBuild2); //輸出： hello Beautiful world
+
+            //將某個位置的取多少字元刪除（從0開始算，到第2個字元開始刪4個字元）
+            stringBuild1.Remove(2, 4);
+            Console.WriteLine(stringBuild1);  //輸出：ha
+
+            //取代
+            stringBuild1.Replace("h", "k");
+            Console.WriteLine(stringBuild1);  //輸出：ka
+
+            //
+            //將 StringBuilder 物件轉換為字串
+            //
+
+            StringBuilder sb = new StringBuilder();
+            string[] spellings = { "recieve", "receeve", "receive" };
+            sb.AppendFormat("Which of the following spellings is {0}:", true);
+
+            //加入斷行
+            sb.AppendLine();
+
+            for (int ctr = 0; ctr <= spellings.GetUpperBound(0); ctr++)
+            {
+                sb.AppendFormat("   {0}. {1}", ctr, spellings[ctr]);
+                sb.AppendLine();
+            }
+
+            sb.AppendLine();
+
+            //有著斷行的並非直接是字串，所以要用ToString()去把他變成字串
+            Console.WriteLine(sb.ToString());
+
+            //輸出：
+            //       Which of the following spellings is True:
+            //          0. recieve
+            //          1. receeve
+            //          2. receive
 
 
             Console.Read();
