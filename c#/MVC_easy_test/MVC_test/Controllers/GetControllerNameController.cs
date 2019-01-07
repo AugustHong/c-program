1772a4c9804c96cdd189052ffa6bde84604a4645
hitler11319 <hitler11319@gmail.com>
2019-01-07 20:48:43 +0800

    MVC_easy_test(欄位驗證、抽換邏輯、分層、AutoMapper、Dapper、HtmlHelper、Filter(執行前、執行後)、Filter其他應用、上傳資料夾、API介紹與取值、從View和Controller中取到Controller、Action、Area的

---
+++ c#/MVC_easy_test/MVC_test/Controllers/GetControllerNameController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_test.Controllers
+{
+	public class GetControllerNameController : Controller
+	{
+		// GET: GetControllerName
+		public ActionResult Index()
+		{
+
+			//在Controller中取得Area、Controller、Action的名稱
+			ViewBag.AreaName = ControllerContext.RouteData.DataTokens["area"];   //若沒使用則為null
+			ViewBag.ControllerName = ControllerContext.RouteData.Values["controller"];
+			ViewBag.ActionName = ControllerContext.RouteData.Values["action"];
+
+			return View();
+		}
+	}
+}
