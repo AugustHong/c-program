f4f95d84cb610c823c12e33281222534063f226d
hitler11319 <hitler11319@gmail.com>
2019-01-16 20:45:41 +0800

    驗證碼簡單應用、用Temp+Layout實現每頁都能alert()、PartialView的介紹、多國語系

--- c#/MVC_easy_test_2/MVC_easy_test_2/App_Start/RouteConfig.cs
+++ c#/MVC_easy_test_2/MVC_easy_test_2/App_Start/RouteConfig.cs
@@ -7,17 +7,27 @@ using System.Web.Routing;
 
 namespace MVC_easy_test_2
 {
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+	public class RouteConfig
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
-        }
-    }
+			//因為多國語系，所以路徑改為  語系/Controller/Action
+			//預設沒有語系時的路徑（因為語系如果是空的=>預設中文）
+			routes.MapRoute(
+					name: "Localized",
+					url: "{lang}/{controller}/{action}/{id}",
+					constraints: new { lang = @"(\w{2})|(\w{2}-\w{2})" },
+					defaults: new { controller = "Captcha", action = "Index", id = UrlParameter.Optional }
+			);
+
+			
+			routes.MapRoute(
+				name: "Default",
+				url: "{controller}/{action}/{id}",
+				defaults: new { controller = "Captcha", action = "Index", id = UrlParameter.Optional }
+			);
+		}
+	}
 }
