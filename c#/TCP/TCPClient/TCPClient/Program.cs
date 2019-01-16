e278105f8979e7ba76c858b11479e951d1f438ee
hitler11319 <hitler11319@gmail.com>
2019-01-16 19:56:53 +0800

    TCP   Server和Client用Socket溝通

---
+++ c#/TCP/TCPClient/TCPClient/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;  //要引用這一隻
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+	參考網址：https://dotblogs.com.tw/atowngit/2010/01/16/13047 
+*/
+
+namespace TCPClient
+{
+	public static class Program
+	{
+		//宣告網路資料流變數
+		public static NetworkStream myNetworkStream;
+
+		//宣告 Tcp 用戶端物件
+		public static TcpClient myTcpClient;
+
+		static void Main(string[] args)
+		{
+			Console.Write("輸入連接機名稱 : ");
+			//取得主機名稱
+			string hostName = Console.ReadLine();
+
+			Console.Write("輸入連接通訊埠 : ");
+			//取得連線 IP 位址
+			int connectPort = int.Parse(Console.ReadLine());
+
+			//建立 TcpClient 物件
+			myTcpClient = new TcpClient();
+
+
+			try
+			{
+				//測試連線至遠端主機
+				myTcpClient.Connect(hostName, connectPort);
+
+				//建立資料流
+				myNetworkStream = myTcpClient.GetStream();
+
+				Console.WriteLine("連線成功 !!");
+			}
+			catch
+			{
+				Console.WriteLine("主機 {0} 通訊埠 {1} 無法連接  !!", hostName, connectPort);
+				return;
+			}
+
+			//開始進行作業，第一個傳出去的是我的ip
+			WriteData(GetLocalIP(true).FirstOrDefault());
+
+			Console.Read();
+		}
+
+		//寫入資料
+		public  static void WriteData()
+		{
+			Console.Write("請輸入你要傳送的文字： ");
+			String strTest = Console.ReadLine();
+
+			//將字串轉 byte 陣列，使用 UTF-8 編碼
+			Byte[] myBytes = Encoding.UTF8.GetBytes(strTest);
+
+			//將字串寫入資料流
+			myNetworkStream.Write(myBytes, 0, myBytes.Length);
+
+			//寫完後就等著收資料了
+			ReadData();
+		}
+
+		//寫入資料（傳參數）
+		public static void WriteData(string input)
+		{
+			//將字串轉 byte 陣列，使用 UTF-8 編碼
+			Byte[] myBytes = Encoding.UTF8.GetBytes(input);
+
+			//將字串寫入資料流
+			myNetworkStream.Write(myBytes, 0, myBytes.Length);
+
+			//寫完後就等著收資料了
+			ReadData();
+		}
+
+		//讀取資料
+		public static void ReadData()
+		{
+			//從網路資料流讀取資料
+			int bufferSize = myTcpClient.ReceiveBufferSize;
+
+			//資料流接收文字並讀取
+			byte[] receiveDataByte = new byte[bufferSize];
+			myNetworkStream.Read(receiveDataByte, 0, bufferSize);
+
+			//取得資料並且解碼文字（用 \0是多餘的位元數）
+			string receiveData = Encoding.UTF8.GetString(receiveDataByte, 0, bufferSize).Replace("\0", "");
+			Console.Write("從Server中送過來的資料為：" + receiveData + "\n");
+
+			//收完資料，繼續回到輸入資料
+			WriteData();
+		}
+
+
+		/*
+		 來自於我github中的c-program/c#/Hong.IPHelper的method
+		 */
+
+		/// <summary>
+		/// 取得LocalIP
+		/// </summary>
+		/// <param name="onlyIPV4">是否只取IPV4（預設為false）</param>
+		/// <returns></returns>
+		public static List<string> GetLocalIP(bool onlyIPV4 = false)
+		{
+			//先取得主機名稱
+			string hostName = Dns.GetHostName();
+
+			List<string> result = new List<string>();
+
+			IPHostEntry iphostentry = Dns.GetHostByName(hostName);   //取得本機的 IpHostEntry 類別實體
+
+			foreach (var ip in iphostentry.AddressList)
+			{
+				//是否只取得IPV4
+				//第一段判別式是判別是否是IPV4，所以其他的都會是false（故用or來加入 => 當後面是true則是取全部資料）
+				//當後面是false是只取IPV4的資料
+				if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork || !onlyIPV4)
+				{
+					result.Add(ip.ToString());
+				}
+
+			}
+
+			return result;
+		}
+
+	}
+}
---
+++ c#/TCP/TCPServer/TCPServer/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;  //要引用這隻
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPServer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//IP（自訂）
+			string IP = "127.0.0.1";
+
+			//Port（自訂）
+			int port = 36000;
+
+			//接收的長度上限（自訂）
+			int receiveLen = 2048;
+
+			//建立 IPAddress 物件(本機)
+			System.Net.IPAddress theIPAddress = System.Net.IPAddress.Parse(IP);
+
+			//建立監聽物件（剛才建立的IPAddress物件, port）
+			TcpListener myTcpListener = new TcpListener(theIPAddress, port);
+
+			//啟動監聽
+			myTcpListener.Start();
+
+			Console.WriteLine("TCP Server啟動  \n通訊埠 36000 \n等待用戶端連線...... !!");
+
+			//建立Socket（要接收監聽到的值，和回傳用的）
+			Socket mySocket = myTcpListener.AcceptSocket();
+
+			do
+			{
+				try
+				{
+					//偵測是否有來自用戶端的連線要求，若是
+					//用戶端請求連線成功，就會秀出訊息。
+					if (mySocket.Connected)
+					{
+						//建立byte[]來接收值（但會過長，可能有多餘的空格。而下面在送給Client時就會出現一大堆空格）
+						byte[] ReceiveDataByte = new byte[receiveLen];
+
+						//取得用戶端寫入的資料長度
+						int dataLength = mySocket.Receive(ReceiveDataByte);
+
+						//實際取得資料（去掉後面的一大堆空白）
+						byte[] trueReceiveDataByte = new byte[dataLength];
+
+						//複製取得的資料，並放入到真正長度的byte[]中
+						//把他複製到trueReceiveDataByte（分別為從ReceiveDataByte的第0位置開始  copy共len個位數資料 到 trueReceiveDataByte 的第0位數）
+						Array.Copy(ReceiveDataByte, 0, trueReceiveDataByte, 0, dataLength);
+
+						Console.WriteLine("接收到的資料長度 {0} \n ", dataLength.ToString());
+						Console.Write("取出用戶端寫入網路資料流的資料內容 :");
+
+						//取出資料  (用 \0是多餘的位元數，再度檢查一次)
+						string ReceiveData = Encoding.UTF8.GetString(trueReceiveDataByte, 0, dataLength).Replace("\0", "");
+						Console.Write(ReceiveData + "\n");
+
+						//將接收到的資料回傳給用戶端
+						//mySocket.Send(byte[]資料, size, flag)
+						mySocket.Send(trueReceiveDataByte, trueReceiveDataByte.Length, 0);
+					}
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e.Message);
+					mySocket.Close();
+					break;
+				}
+
+			} while (true);
+		}
+	}
+}
