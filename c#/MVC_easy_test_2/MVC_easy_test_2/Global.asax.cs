fba1299210633783a54706193b6581964e850c80
hitler11319 <hitler11319@gmail.com>
2019-02-11 20:53:07 +0800

    驗證碼簡單應用、用Temp+Layout實現每頁都能alert()、PartialView的介紹、多國語系、NLog、在Global寫Web排程

--- c#/MVC_easy_test_2/MVC_easy_test_2/Global.asax.cs
+++ c#/MVC_easy_test_2/MVC_easy_test_2/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.Globalization;     //要記得引用（多國語系所需要的）
 using System.Linq;
@@ -15,7 +16,40 @@ namespace MVC_easy_test_2
 		{
 			AreaRegistration.RegisterAllAreas();
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+
+			//寫Web上的排程(一定要在Application_Start這隻Function中)
+			//啟用多執行緒(TaskLoop是下方的函式)
+			ThreadStart tsTask = new ThreadStart(TaskLoop);
+			Thread MyTask = new Thread(tsTask);
+			MyTask.Start();
 		}
+
+
+		/// <summary>
+		/// 執行排程內的Loop
+		/// </summary>
+		static void TaskLoop()
+		{
+			while (true)
+			{
+				// 執行的動作都放這(如果有好幾個就寫好幾個吧)，這邊只先寫一個即可
+				ToDo();
+
+				// 等待幾秒後繼續執行排程(這裡是寫500秒)
+				System.Threading.Thread.Sleep(TimeSpan.FromSeconds(500));
+			}
+		}
+
+		//執行的動作，這邊只是示範所以就只寫log即可
+		static void ToDo()
+		{
+			//這邊是做測試用所以才加的
+			Logger logger = LogManager.GetCurrentClassLogger();
+
+			logger.Info("排程正在執行中……");
+	}
+
 
 		//多國語系要用的
 		protected void Application_PostMapRequestHandler(Object sender, EventArgs e)
