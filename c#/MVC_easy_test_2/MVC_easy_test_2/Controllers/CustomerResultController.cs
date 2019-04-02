af26a8bc8b5621156b150e4505369d152889bc33
hitler11319 <hitler11319@gmail.com>
2019-04-02 21:50:58 +0800

    驗證碼簡單應用、用Temp+Layout實現每頁都能alert()、PartialView的介紹、多國語系、NLog、在Global寫Web排程、權限控管、CodeTemplates範本、依據300還是404…等導去不同的頁面(Global裡寫)、客製化Result

---
+++ c#/MVC_easy_test_2/MVC_easy_test_2/Controllers/CustomerResultController.cs
@@ -0,0 +1,51 @@
+﻿using MVC_easy_test_2.Infrastructure.CustomResults;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+/*
+	客製化Result
+	步驟1：先建立自己的客製化Result (詳見 Infrastructure/CustomResults/ExportExcelResult.cs)
+	步驟2：引用，並return 它
+*/
+
+namespace MVC_easy_test_2.Controllers
+{
+	/// <summary>
+	/// 客製化Result
+	/// </summary>
+    public class CustomerResultController : Controller
+    {
+        // GET: CustomerResult
+        public ActionResult Index()
+        {
+			//這裡要使用我們的客製化Result
+			//他是一個 變成 下載 Excel 的東西 => 所以要先準備資料
+			JArray jObjects = new JArray();    //一個Json 的 物件 Array
+			for(int i = 0; i <= 10; i++)
+			{
+				JObject jo = new JObject();
+				jo.Add("流水號", i);
+				jObjects.Add(jo);
+			}
+
+			//轉成JSON格式(要先ToString)
+			DataTable dt = JsonConvert.DeserializeObject<DataTable>(jObjects.ToString());
+
+			//檔名
+			string exportFileName = string.Concat("export", DateTime.Now.ToString("yyyyMMddHHmm"),".xlsx");
+
+			//呼叫我們的客製化Result
+			return new ExportExcelResult{
+				SheetName = "Export",
+				FileName = exportFileName,
+				ExportData = dt
+			};
+		}
+    }
+}
