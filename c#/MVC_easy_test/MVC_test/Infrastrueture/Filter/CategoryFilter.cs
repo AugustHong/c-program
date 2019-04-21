1b3a95976af1c5309b53bdb47db67959cc634d58
hitler11319 <hitler11319@gmail.com>
2019-04-21 19:32:47 +0800

    MVC_easy_test(欄位驗證、抽換邏輯、分層、AutoMapper、Dapper、HtmlHelper、Filter(執行前、執行後)、Filter其他應用、上傳資料夾、API介紹與取值、從View和Controller中取到Controller、Action、Area的

--- c#/MVC_easy_test/MVC_test/Infrastrueture/Filter/CategoryFilter.cs
+++ c#/MVC_easy_test/MVC_test/Infrastrueture/Filter/CategoryFilter.cs
@@ -15,58 +15,74 @@ using System.Web.Routing;
 
 namespace MVC_test.Infrastrueture.Filter
 {
-    /// <summary>
-    /// 取得所有種類的下拉式清單
-    /// </summary>
-    public class CategoryFilter : ActionFilterAttribute
-    {
-        //覆寫其ActionFilterAttribute的函式（這函式是action一開始執行後執行的）
-        //ActionExecutedContext就是只在執行這action（controller裡的），所有的相關資料集合
-        public override void OnActionExecuted(ActionExecutedContext filterContext)
-        {
-            //把東西載入到OnActionExecuted（一定要寫的）
-            base.OnActionExecuted(filterContext);
+	/// <summary>
+	/// 取得所有種類的下拉式清單
+	/// </summary>
+	public class CategoryFilter : ActionFilterAttribute
+	{
+		//覆寫其ActionFilterAttribute的函式（這函式是action一開始執行後執行的）
+		//ActionExecutedContext就是只在執行這action（controller裡的），所有的相關資料集合
+		public override void OnActionExecuted(ActionExecutedContext filterContext)
+		{
+			//把東西載入到OnActionExecuted（一定要寫的）
+			base.OnActionExecuted(filterContext);
 
-            //取得所有種類（照理來說是要寫成service，但是懶得寫所以寫在這裡）
-            string connString = System.Configuration.ConfigurationManager.ConnectionStrings["dbContext"].ToString();
+			//取得所有種類（照理來說是要寫成service，但是懶得寫所以寫在這裡）
+			string connString = System.Configuration.ConfigurationManager.ConnectionStrings["dbContext"].ToString();
 
-            var sql = "select * from Categories order by ID";
+			var sql = "select * from Categories order by ID";
 
-            //用using會讓裡面的conn連線用完會自動斷線（即用完就結束）
-            using (var conn = new SqlConnection(connString))
-            {
-                var data = conn.Query<CategoryViewModel>(sql);  //Dapper用的 
+			//用using會讓裡面的conn連線用完會自動斷線（即用完就結束）
+			using (var conn = new SqlConnection(connString))
+			{
+				var data = conn.Query<CategoryViewModel>(sql);  //Dapper用的 
 
-                // 放入ViewData/ViewBag
-                filterContext.Controller.ViewBag.CategoryList = data;
-            }
-        }
+				// 放入ViewData/ViewBag
+				filterContext.Controller.ViewBag.CategoryList = data;
+			}
+		}
 
-        //執行前執行的
-        public override void OnActionExecuting(ActionExecutingContext filterContext)
-        {
+		//執行前執行的
+		public override void OnActionExecuting(ActionExecutingContext filterContext)
+		{
+			//判別是否是哪一個Controller
+			bool isCategoryController = filterContext.Controller is CategoryController;
 
-            //判別是否是哪一個Controller
-            bool isCategoryController = filterContext.Controller is CategoryController;
+			//判斷這個Action是否有這個Attribute（後面的CategoryFilter是Attribute名）
+			bool isAttribute = filterContext.ActionDescriptor.IsDefined(typeof(CategoryFilter), true);
 
-            //判斷這個Action是否有這個Attribute（後面的CategoryFilter是Attribute名）
-            bool isAttribute = filterContext.ActionDescriptor.IsDefined(typeof(CategoryFilter), true);
+			//跳轉頁面（可以做判斷，如果沒登入就進去Login畫面）
+			//其中Redirect是下面的函式
+			//filterContext.Result = Redirect("Account", "LogIn");
 
-            //跳轉頁面（可以做判斷，如果沒登入就進去Login畫面）
-            //其中Redirect是下面的函式
-            //filterContext.Result = Redirect("Account", "LogIn");
+			base.OnActionExecuting(filterContext);
+		}
 
+		//做跳轉頁面用
+		private RedirectToRouteResult Redirect(string controller, string action)
+		{
+			return new RedirectToRouteResult(new RouteValueDictionary {
+											  { "Controller", controller },
+											  { "Action", action }}
+			);
+		}
 
-            base.OnActionExecuting(filterContext);
-        }
+		//一樣做跳轉頁面使用(但有加上area的)
+		private RedirectToRouteResult Redirect(string controller, string action, string area = null)
+		{
+			return new RedirectToRouteResult(new RouteValueDictionary {
+				{ "Controller", controller },
+				{ "Action", action },
+				{ "Area", area} });
+		}
+	}
 
-        //做跳轉頁面用
-        private RedirectToRouteResult Redirect(string controller, string action)
-        {
-            return new RedirectToRouteResult(new RouteValueDictionary {
-                                                                    { "Controller", controller },
-                                                                    { "Action", action }}
-            );
-        }
-    }
+	//Filter接2個參數以上的值
+	public class TestFilter : ActionFilterAttribute
+	{
+		//參數值傳入
+		//在Controller中就是這樣使用 [Test(A="aaa",B="bbb")]  這樣的使用法
+		public string A { get; set; }
+		public string B { get; set; }
+	}
 }
