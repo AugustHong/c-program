f12d32a1aee538956a1bf05c62dc3dbc79073e50
hitler11319 <hitler11319@gmail.com>
2019-08-27 21:35:36 +0800

    取得當月第一天和最後一天+取得當月共幾天

---
+++ c#/CurrentMonthFirstDayAndLastDay/CurrentMonthFirstDayAndLastDay/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrentMonthFirstDayAndLastDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //參考網圵： https://dotblogs.com.tw/codeman/archive/2011/07/29/32281.aspx
+            //取到當月的第一天和最後一天
+            DateTime FirstDay = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+            DateTime LastDay = new DateTime(DateTime.Now.AddMonths(1).Year, DateTime.Now.AddMonths(1).Month, 1).AddDays(-1);
+
+            //算出這個月有幾天
+            String yy = DateTime.Now.Year.ToString();
+            String mm = DateTime.Now.Month.ToString();
+            String days = DateTime.DaysInMonth(int.Parse(yy), int.Parse(mm)).ToString();
+
+            Console.WriteLine(FirstDay);
+            Console.WriteLine(LastDay);
+            Console.WriteLine(days);
+
+            Console.ReadLine();
+        }
+    }
+}
