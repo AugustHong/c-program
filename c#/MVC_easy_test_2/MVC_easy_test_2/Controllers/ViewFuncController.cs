c21731391c5b584815754b4abcedd5efea90a535
hitler11319 <hitler11319@gmail.com>
2019-12-22 20:11:51 +0800

    驗證碼簡單應用、用Temp+Layout實現每頁都能alert()、PartialView的介紹、多國語系、NLog、在Global寫Web排程、權限控管、CodeTemplates範本、依據300還是404…等導去不同的頁面(Global裡寫)、客製化Result、ViewFu

---
+++ c#/MVC_easy_test_2/MVC_easy_test_2/Controllers/ViewFuncController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+/*
+	ViewFunc： 就是在 View 中可以使用 C# 的函式
+
+	步驟1： 建立 App_Code 資料夾 (名字要一模一樣)
+	步驟2： 在 App_Code 資料夾 新增 ViewFunc.cshtml 的檔案 (名字也一樣)
+	步驟3： 實作
+*/
+
+namespace MVC_easy_test_2.Controllers
+{
+    public class ViewFuncController : Controller
+    {
+        // GET: ViewFunc
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
