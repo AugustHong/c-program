a26f53ad977d00a4577427bba7eb728977e366f9
hitler11319 <hitler11319@gmail.com>
2020-01-01 19:53:41 +0800

    驗證碼應用、用Temp+Layout實現每頁都能alert()、PartialView介紹、多國語系、NLog、在Global寫Web排程、權限控管、CodeTemplates範本、依據300還是404…等導去不同的頁面(Global裡寫)、客製化Result、ViewFunc

--- c#/MVC_easy_test_2/MVC_easy_test_2/Controllers/PartialViewController.cs
+++ c#/MVC_easy_test_2/MVC_easy_test_2/Controllers/PartialViewController.cs
@@ -13,5 +13,22 @@ namespace MVC_easy_test_2.Controllers
         {
             return View();
         }
-    }
+
+		// 實作另一種PartialView ， 可以在裡面先處理
+		// 不過 視同 一般的一樣， 要有個 和 Action 相同名稱的 cshtml 檔 在這個 Controller 下
+		public ActionResult PartialTest()
+		{
+			ViewBag.Test = "Test";
+
+			// 這邊不是 return View() 喔
+			return PartialView();
+
+			/*
+				呼叫方法：(一定要用 @{} 包起來)
+						@{ 
+							Html.RenderAction("PartialTest", "PartialView");
+						}
+			*/
+		}
+	}
 }
