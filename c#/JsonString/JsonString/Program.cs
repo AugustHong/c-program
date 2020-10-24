682264ae6d534ac2c3c2a7854bf30da562f27838
hitler11319 <hitler11319@gmail.com>
2020-10-24 20:26:48 +0800

    轉成JSON字串+和JSON字串轉成Class 和 MVC中Controller和View互傳JSON字串+JObject轉Model+JSON格式化

--- c#/JsonString/JsonString/Program.cs
+++ c#/JsonString/JsonString/Program.cs
@@ -126,26 +126,31 @@ namespace JsonString
 		/// <returns></returns>
 		static string FormatJson(string str)
 		{
-			//格式化json字串
-			JsonSerializer serializer = new JsonSerializer();
-			TextReader tr = new StringReader(str);
-			JsonTextReader jtr = new JsonTextReader(tr);
-			object obj = serializer.Deserialize(jtr);
-			if (obj != null)
-			{
-				StringWriter textWriter = new StringWriter();
-				JsonTextWriter jsonWriter = new JsonTextWriter(textWriter)
+			try{
+				//格式化json字串
+				JsonSerializer serializer = new JsonSerializer();
+				TextReader tr = new StringReader(str);
+				JsonTextReader jtr = new JsonTextReader(tr);
+				object obj = serializer.Deserialize(jtr);
+				if (obj != null)
 				{
-					Formatting = Formatting.Indented,
-					Indentation = 4,
-					IndentChar = ' '
-				};
-				serializer.Serialize(jsonWriter, obj);
-				return textWriter.ToString();
+					StringWriter textWriter = new StringWriter();
+					JsonTextWriter jsonWriter = new JsonTextWriter(textWriter)
+					{
+						Formatting = Formatting.Indented,
+						Indentation = 4,
+						IndentChar = ' '
+					};
+					serializer.Serialize(jsonWriter, obj);
+					return textWriter.ToString();
+				}
+				else
+				{
+					return str;
+				}
 			}
-			else
-			{
-				return str;
+			catch (Exception ex){
+				return string.Empty;
 			}
 		}
 	}
