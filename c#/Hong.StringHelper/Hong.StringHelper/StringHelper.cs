bbdcaba6edbaa301f0538e3af5d98ccdc0aef890
hitler11319 <hitler11319@gmail.com>
2020-10-24 20:28:33 +0800

    Hong.StringHelper(有try catch的substring + 轉全形 + 回傳固定長度的字串+ 字串比大小 + try parse 簡寫 + 驗證全英文 全數字 全英數字 全中文 有包含中文 + 用字串分割字串 + 重複字串 + 無條件捨去至小數第幾位 + 數字和小數格式 + Trim)

--- c#/Hong.StringHelper/Hong.StringHelper/StringHelper.cs
+++ c#/Hong.StringHelper/Hong.StringHelper/StringHelper.cs
@@ -850,25 +850,30 @@ namespace Hong.StringHelper
 		/// <returns></returns>
 		static string FormatJson(string str)
 		{
-			//格式化json字串
-			JsonSerializer serializer = new JsonSerializer();
-			TextReader tr = new StringReader(str);
-			JsonTextReader jtr = new JsonTextReader(tr);
-			object obj = serializer.Deserialize(jtr);
-			if (obj != null)
-			{
-				StringWriter textWriter = new StringWriter();
-				JsonTextWriter jsonWriter = new JsonTextWriter(textWriter)
+			try{
+				//格式化json字串
+				JsonSerializer serializer = new JsonSerializer();
+				TextReader tr = new StringReader(str);
+				JsonTextReader jtr = new JsonTextReader(tr);
+				object obj = serializer.Deserialize(jtr);
+				if (obj != null)
 				{
-					Formatting = Formatting.Indented,
-					Indentation = 4,
-					IndentChar = ' '
-				};
-				serializer.Serialize(jsonWriter, obj);
-				return textWriter.ToString();
+					StringWriter textWriter = new StringWriter();
+					JsonTextWriter jsonWriter = new JsonTextWriter(textWriter)
+					{
+						Formatting = Formatting.Indented,
+						Indentation = 4,
+						IndentChar = ' '
+					};
+					serializer.Serialize(jsonWriter, obj);
+					return textWriter.ToString();
+				}
+				else
+				{
+					return str;
+				}
 			}
-			else
-			{
+			catch (Exception ex){
 				return str;
 			}
 		}
