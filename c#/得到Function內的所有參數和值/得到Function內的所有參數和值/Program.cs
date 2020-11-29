7f1bb34ad407b9c2a29801033d90705ba0257ddd
hitler11319 <hitler11319@gmail.com>
2020-11-29 19:19:04 +0800

    得到 Function 內的所有參數名稱+Type

--- c#/得到Function內的所有參數和值/得到Function內的所有參數和值/Program.cs
+++ c#/得到Function內的所有參數和值/得到Function內的所有參數和值/Program.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 
 /*
     參考網圵： https://bbs.csdn.net/topics/390797738 
+    參考網圵： https://dotblogs.com.tw/H20/2017/12/05/121148
 */
 
 namespace 得到Function內的所有參數和值
@@ -41,7 +42,7 @@ namespace 得到Function內的所有參數和值
             // 這邊可以取到 此DoSomething 函式裡的 所有變數的名稱 和 type
             StackTrace t = new StackTrace();
             StackFrame[] frames = t.GetFrames();
-            MethodBase method = frames[0].GetMethod();
+            MethodBase method = frames[0].GetMethod();  // 如果要取到上一個函式 用 frames[1].GetMethod() 後面會說到
 
             ParameterInfo[] infos = method.GetParameters();
 
@@ -62,6 +63,75 @@ namespace 得到Function內的所有參數和值
              */
 
             output = "Success";
+        }
+
+        /// <summary>
+        /// 取得 目前正在執行的 Function Info 資訊
+        /// </summary>
+        /// <returns></returns>
+        public static String GetCurrentMethodInfo()
+        {
+            string showString = "";
+            //取得當前方法類別命名空間名稱
+
+            showString += "Namespace:" + System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Namespace + "\n";
+            //取得當前類別名稱
+
+            showString += "class Name:" + System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.FullName + "\n";
+            //取得當前所使用的方法
+
+            showString += "Method:" + System.Reflection.MethodBase.GetCurrentMethod().Name + "\n";
+
+            return showString;
+        }
+
+        /// <summary>
+        /// 取得父類別的相關資訊(共用的Functiond可用)
+        /// 用法類型 DoSomething 函式裡的 (所以才說 可以得到呼叫的上一層函式)
+        /// 例如： A函式 呼叫此函式 會得到 A 函式的資料
+        /// 註： 而且 如果寫在 Exception 中的話， 某個函式 throw new 你建的Exception ，就可以在 Exception 得到是哪個函式的資料了
+        /// </summary>
+        /// <returns></returns>
+        public static String GetParentInfo()
+        {
+            String showString = "";
+            StackTrace ss = new StackTrace(true);
+
+            //取得呼叫當前方法之上一層類別(GetFrame(1))的屬性
+            MethodBase mb = ss.GetFrame(1).GetMethod();
+
+            //取得呼叫當前方法之上一層類別(父方)的命名空間名稱
+            showString += mb.DeclaringType.Namespace + "\n";
+
+            //取得呼叫當前方法之上一層類別(父方)的function 所屬class Name
+            showString += mb.DeclaringType.Name + "\n";
+
+            //取得呼叫當前方法之上一層類別(父方)的Full class Name
+            showString += mb.DeclaringType.FullName + "\n";
+
+            //取得呼叫當前方法之上一層類別(父方)的Function Name
+            showString += mb.Name + "\n";
+
+            return showString;
+        }
+    }
+
+    /// 得到 函式 名稱的函式 (就是用 GetParentInfo() 來寫的 )
+    /// 因為 別人呼叫此 Helper 的話，就得要得到上一層 Func 的資料才對
+    /// 這邊只寫一個做範例，之後視情況自己照上面的內容擴充
+    public static class GetFuncInfoHelper{
+        public static string GetFuncName(){
+            string result = string.Empty;
+
+            StackTrace ss = new StackTrace(true);
+
+            //取得呼叫當前方法之上一層類別(GetFrame(1))的屬性
+            MethodBase mb = ss.GetFrame(1).GetMethod();
+
+            //取得呼叫當前方法之上一層類別(父方)的Function Name
+            result = mb.Name;
+
+            return result;
         }
     }
 }
