ecaf8e4b2c36adcaf869809de020816c74a8b80e
hitler11319 <hitler11319@gmail.com>
2020-12-30 20:15:23 +0800

    Hong.AutoMapperHelper (Owner:Hong) (AutoMapper簡單應用)

---
+++ c#/Hong.AutoMapperHelper/Hong.AutoMapperHelper/AutoMapperHelper.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hong.AutoMapperHelper
+{
+    public static class AutoMapperHelper
+    {
+        /// <summary>
+        /// AutoMapper
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="u"></param>
+        /// <returns></returns>
+        public static T Convert<U, T>(this U u)
+        {
+            try
+            {
+                var config = new MapperConfiguration(cfg => cfg.CreateMap<U, T>());
+                var mapper = config.CreateMapper();
+                return mapper.Map<T>(u);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                return default(T);
+            }
+        }
+    }
+}
