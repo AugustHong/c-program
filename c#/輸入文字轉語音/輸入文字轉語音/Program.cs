4e405aae506d9e5b2d19ccebb1e0c1bb5226b831
hitler11319 <hitler11319@gmail.com>
2022-08-24 20:48:41 +0800

    輸入文字轉語音(目前只有英數字，中文無法)

---
+++ c#/輸入文字轉語音/輸入文字轉語音/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+/*
+	參考網址： https://www.howcando.net/ask/79734.html
+      參考右鍵/加入參考/找到 System.Speech
+ */
+
+namespace 輸入文字轉語音
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string readText = string.Empty;
+
+			// 只能 英數字而己 (中文好像要裝什麼包)
+			SpeechSynthesizer reader = new SpeechSynthesizer();
+			reader.Volume = 100;  // 0...100
+
+
+			while (readText.ToUpper() != "EXIT")
+			{
+				Console.WriteLine("請輸入文字：");
+				readText = Console.ReadLine();
+
+				if (!string.IsNullOrWhiteSpace(readText))
+				{
+					reader.SpeakAsync(readText);
+				}
+			}
+		}
+	}
+}
