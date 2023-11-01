9888257c3904a8a2289c2f28ef8aca4bd237754a
hitler11319 <hitler11319@gmail.com>
2023-11-01 19:46:30 +0800

    鍵盤監測

---
+++ c#/鍵盤監測/鍵盤監測/KeyboardHook.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace 鍵盤監測
+{
+    /// <summary>
+    /// 鍵盤鉤子 (Hook ， 要監控都會掛載 Hook)
+    /// 以下是參考 https://www.cnblogs.com/ayqy/p/3636427.html ，非本人原創寫法。供自己參考學習用
+    /// </summary>
+    public class KeyboardHook
+    {
+        // 宣告告事件
+        public event KeyEventHandler KeyDownEvent;
+        public event KeyPressEventHandler KeyPressEvent;
+        public event KeyEventHandler KeyUpEvent;
+
+        public delegate int HookProc(int nCode, Int32 wParam, IntPtr lParam);  // 宣告處理動作的Delegate
+        static int hKeyboardHook = 0; //鍵盤Hook初始值
+
+        public const int WH_KEYBOARD_LL = 13;   //Local鍵盤監聽滑乘設為2，Global鍵盤監聽滑乘設為13 (詳細值可在 Microsoft SDK的Winuser.h裡查询)
+        HookProc KeyboardHookProcedure; //宣告KeyboardHookProcedure作為HookProc類型
+
+        //鍵盤結構
+        [StructLayout(LayoutKind.Sequential)]
+        public class KeyboardHookStruct
+        {
+            public int vkCode;  //定義1個虛擬鍵碼。範圍1至254
+            public int scanCode; // 指定硬體掃描碼
+            public int flags;  // 標誌
+            public int time; // 訊息時間戳記
+            public int dwExtraInfo; // 指定額外訊息
+        }
+
+
+        //使用此功能，安裝Hook
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern int SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hInstance, int threadId);
+
+        //使用此功能，解安裝Hook
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern bool UnhookWindowsHookEx(int idHook);
+
+        //使用此功能，繼續取得下一個Hook
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern int CallNextHookEx(int idHook, int nCode, Int32 wParam, IntPtr lParam);
+
+        // 取得當前程序編號(Local Hook 需要)
+        [DllImport("kernel32.dll")]
+        static extern int GetCurrentThreadId();
+
+        //使用WINDOWS API函式取代當前實作的函式，防止Hook失效
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetModuleHandle(string name);
+
+        // 開始動作
+        public void Start()
+        {
+            // 如果 Hook 是初始值，去安裝Hook
+            if (hKeyboardHook == 0)
+            {
+                KeyboardHookProcedure = new HookProc(KeyboardHookProc);  // 給 Delegate 宣告實體
+
+                // 安裝Hook
+                hKeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardHookProcedure, GetModuleHandle(System.Diagnostics.Process.GetCurrentProcess().MainModule.ModuleName), 0);
+
+                // 如果裝完Hook，值還是初始值 => 安裝失敗
+                if (hKeyboardHook == 0)
+                {
+                    Stop();
+                    throw new Exception("安裝 Hook 失敗");
+                }
+            }
+        }
+
+        // 停止動作
+        public void Stop()
+        {
+            bool retKeyboard = true;
+
+            // 若非 初始值 -> 可以解除安裝
+            if (hKeyboardHook != 0)
+            {
+                // 解除安裝 Hook
+                retKeyboard = UnhookWindowsHookEx(hKeyboardHook);
+                hKeyboardHook = 0;
+            }
+
+            // 若 解除安裝失敗
+            if (!(retKeyboard)) throw new Exception("解除安裝Hook失敗");
+        }
+
+
+        //ToAscii轉換鍵盤的值變成
+        [DllImport("user32")]
+        public static extern int ToAscii(int uVirtKey, int uScanCode, byte[] lpbKeyState, byte[] lpwTransKey, int fuState);
+
+        //取得鍵盤狀態
+        [DllImport("user32")]
+        public static extern int GetKeyboardState(byte[] pbKeyState);
+
+        // 取得按鍵狀態
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        private static extern short GetKeyState(int vKey);
+
+        // 宣告各常數
+        private const int WM_KEYDOWN = 0x100;//KEYDOWN
+        private const int WM_KEYUP = 0x101;//KEYUP
+        private const int WM_SYSKEYDOWN = 0x104;//SYSKEYDOWN
+        private const int WM_SYSKEYUP = 0x105;//SYSKEYUP
+
+        // 實際處理動作
+        private int KeyboardHookProc(int nCode, Int32 wParam, IntPtr lParam)
+        {
+            // 監聽鍵盤事件
+            if ((nCode >= 0) && (KeyDownEvent != null || KeyUpEvent != null || KeyPressEvent != null))
+            {
+                KeyboardHookStruct MyKeyboardHookStruct = (KeyboardHookStruct)Marshal.PtrToStructure(lParam, typeof(KeyboardHookStruct));
+
+                // raise KeyDown
+                if (KeyDownEvent != null && (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN))
+                {
+                    Keys keyData = (Keys)MyKeyboardHookStruct.vkCode;
+                    KeyEventArgs e = new KeyEventArgs(keyData);
+                    KeyDownEvent(this, e);
+                }
+
+                //按下按鍵
+                if (KeyPressEvent != null && wParam == WM_KEYDOWN)
+                {
+                    byte[] keyState = new byte[256];
+                    GetKeyboardState(keyState);
+
+                    byte[] inBuffer = new byte[2];
+                    // 轉換碼
+                    if (ToAscii(MyKeyboardHookStruct.vkCode, MyKeyboardHookStruct.scanCode, keyState, inBuffer, MyKeyboardHookStruct.flags) == 1)
+                    {
+                        KeyPressEventArgs e = new KeyPressEventArgs((char)inBuffer[0]);
+                        KeyPressEvent(this, e);
+                    }
+                }
+
+                // 按起來
+                if (KeyUpEvent != null && (wParam == WM_KEYUP || wParam == WM_SYSKEYUP))
+                {
+                    Keys keyData = (Keys)MyKeyboardHookStruct.vkCode;
+                    KeyEventArgs e = new KeyEventArgs(keyData);
+                    KeyUpEvent(this, e);
+                }
+            }
+
+            // 如果為1，結束直接回覆。若為 0 則繼續呼叫下一個Hook (在組合鍵時用)
+            return CallNextHookEx(hKeyboardHook, nCode, wParam, lParam);
+        }
+
+        // 初始化
+        ~KeyboardHook()
+        {
+            Stop();
+        }
+    }
+}
---
+++ c#/鍵盤監測/鍵盤監測/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;   //调用WINDOWS API函数时要用到
+using Microsoft.Win32;  //寫入注冊表用
+using System.Windows.Forms;
+using System.Runtime.CompilerServices;
+using System.Drawing;
+
+/*
+    參考網址： https://www.cnblogs.com/ayqy/p/3636427.html
+    先去 參考/右鍵/加入參考/組件/System.Windows.Forms 和 System.Drawing
+ */
+
+namespace 鍵盤監測
+{
+    // 繼承Form 是不要像 Console 一直卡住
+    public class Program : Form
+    {
+        KeyboardHook k_hook;
+        List<int> keyTmpList = new List<int>(); // 因為長壓會一直觸發，想要他真的是接2遍。所以就放入陣列裡，等到Up才取消
+
+        // 釋放控制台
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern bool FreeConsole();
+
+        // 啟動程式
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // 直接啟動
+            Application.Run(new Program());
+        }
+
+        public Program()
+        {
+            // 這邊看你要不要隱藏控制台(因為我是用 Console 建立，但 去繼承 Form 來呈現 WinForm的感覺)
+            // 我這邊是測試用所以就沒隱藏Console (因為我要看結果)
+            //FreeConsole();
+
+            // 設定表單
+            // 目的： 不要讓表單出來 (因為會變成有 Console 和 表單 同時出現在螢幕上)
+            this.Text = "鍵盤監測";
+            this.Size = new Size(0, 0);  // 會剩下上面的工具列
+            this.Visible = false;  // 沒啥作用
+            this.ShowInTaskbar = false;  // 不要出現在工具列中(表單)
+            this.WindowState = FormWindowState.Minimized;  // 設定為最小化
+            this.Hide();  // 設定自己(Form 隱藏) => 沒啥作用
+
+            //安装鍵盤Hook
+            k_hook = new KeyboardHook();
+            k_hook.KeyDownEvent += new KeyEventHandler(hook_KeyDown);//Key Down
+            k_hook.KeyUpEvent += new KeyEventHandler(hook_KeyUp);//Key Up
+            k_hook.Start();//開始動作
+        }
+
+        // 實際 Hook 要執行的動作 (可以把想要處理的寫在這) ，按下時的動作
+        public void hook_KeyDown(object sender, KeyEventArgs e)
+        {
+            bool isUse = false;  // 是否己被觸發 (用於判斷不要重複被觸發到)
+
+            // 一直長壓A，會一直觸發A。 (故寫了1個 KeyUpEvent 來處理)
+            // 若按鍵在 陣列中 -> 代表還沒觸發過 KeyUp -> 代表一直壓著
+            // 並且這樣就可以寫快捷鍵了 (例如 A + D) 因為都會長壓著 A
+            if (keyTmpList.Where(x => x == e.KeyValue).Count() <= 0)
+            {
+                // 先加入陣列中
+                keyTmpList.Add(e.KeyValue);
+
+                // 後面才是主邏輯
+                Console.Write(e.KeyCode + "： ");  // 得到 A, B, LControlKey 這種文字
+
+                // 如果長壓著A，而接下來的是D
+                if (keyTmpList.Contains((int)Keys.A) && e.KeyValue == (int)Keys.D && isUse == false)
+                {
+                    Console.WriteLine("輸入快捷鍵 A + D");
+                    isUse = true;
+                }
+
+                if (e.KeyValue == (int)Keys.LControlKey && isUse == false)
+                {
+                    Console.WriteLine("按下了 左CTRL");
+                    isUse = true;
+                }
+
+                if (e.KeyValue == (int)Keys.RControlKey && isUse == false)
+                {
+                    Console.WriteLine("按下了 右CTRL");
+                    isUse = true;
+                }
+
+                if (e.KeyValue == (int)Keys.F1 && isUse == false)
+                {
+                    Console.WriteLine("按下了 F1");
+                    isUse = true;
+                }
+
+                //判断按下的键（Alt + A）
+                if (e.KeyValue == (int)Keys.A && (int)Control.ModifierKeys == (int)Keys.Alt && isUse == false)
+                {
+                    Console.WriteLine("按下了指定快捷鍵組合 （Alt + A）");
+                    isUse = true;
+                }
+
+                if (e.KeyValue == (int)Keys.A && isUse == false)
+                {
+                    Console.WriteLine("按下了 A");
+                    isUse = true;
+                }
+
+                if (e.KeyValue == (int)Keys.B && isUse == false)
+                {
+                    Console.WriteLine("按下了 B");
+                    isUse = true;
+                }
+
+                if (e.KeyValue == (int)Keys.C && isUse == false)
+                {
+                    Console.WriteLine("按下了 C");
+                    isUse = true;
+
+                    // 結束
+                    k_hook.Stop();
+                    // 關閉程式
+                    this.Close();
+                }
+            }      
+        }
+
+        // Key Up 主要是為了解決長壓的問題
+        public void hook_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (keyTmpList.Where(x => x == e.KeyValue).Count() > 0)
+            {
+                // 將暫存的刪掉
+                keyTmpList.Remove(e.KeyValue);
+            }
+        }
+    }
+}
