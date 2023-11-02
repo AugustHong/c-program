ad97a05a4added21e38864b9d32f7d57ee812f1f
hitler11319 <hitler11319@gmail.com>
2023-11-02 20:03:00 +0800

    呼叫Google圖片並下載

---
+++ c#/呼叫Goole圖片並下載/呼叫Goole圖片並下載/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+    想做到類似輸入文字，就產出圖片
+ */
+
+namespace 呼叫Goole圖片並下載
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://www.google.com.tw/search?q={queryKey}&hl=zh-TW&tbm=isch&source=hp&biw=1280&bih=643&sclient=img";
+            string queryKey = "";
+            string html = "";
+
+            Console.Write("請輸入要查詢的圖片篩選文字：");
+            queryKey = Console.ReadLine();
+
+            url = url.Replace("{queryKey}", queryKey);
+
+            // 取得 網頁內容
+            using (var client = new WebClient())
+            {
+                client.Encoding = Encoding.UTF8;
+                html = client.DownloadString(url);
+            }
+
+            // 開始解析網頁，取出裡面的圖片網址
+            List<string> imgUrlList = new List<string>();
+            if (!string.IsNullOrEmpty(html))
+            {
+                string fKey = "****^^****";
+                string lKey = "****vv****";
+                html = html.Replace("src=\"", fKey).Replace("\" />", lKey).Replace("\"/>", lKey);
+
+                // 開始切割字串
+                List<string> fS = html.Split(fKey);
+
+                // 己看過，要從第3個開始
+                for (int i = 2; i < fS.Count; i++)
+                {
+                    string tmp = fS[i];
+                    List<string> lS = tmp.Split(lKey);
+
+                    // 抓第1個就是網址
+                    string imgUrl = lS[0];
+                    imgUrlList.Add(imgUrl);
+
+                    // 抓前10筆就行
+                    if (imgUrlList.Count >= 10)
+                    {
+                        break;
+                    }
+                }
+
+                // 下載圖檔
+                string appPath = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+                int index = 0;
+                foreach (string imgUrl in imgUrlList)
+                {
+                    using (var client = new WebClient())
+                    {
+                        //創建臨時文件目錄下的存儲必應圖片的絕對路徑
+                        var filePath = appPath + queryKey + (index).ToString() + ".png";
+                        //將圖片下載到這個路徑下
+                        client.DownloadFile(imgUrl, filePath);
+                        Console.WriteLine($"下載圖片 {index} ， 檔名： {filePath}");
+                        index++;
+                    }
+                }
+            }
+
+            Console.WriteLine("==============================己下載完成===========================");
+            Console.Read();
+        }
+    }
+}
---
+++ c#/呼叫Goole圖片並下載/呼叫Goole圖片並下載/StringSplitHelper.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 呼叫Goole圖片並下載
+{
+    public static class StringSplitHelper
+    {
+        /// <summary>
+        /// 可以參考 Hong.StringHelper (這是我之前寫的用 字串分割字串)
+        /// </summary>
+        /// <param name="source">要分被割的字串</param>
+        /// <param name="splitStr">字來分割的字串</param>
+        /// <returns></returns>
+        public static List<string> Split(this string source, string splitStr)
+        {
+            List<string> result = new List<string>();
+
+            string tmpSource = source;
+
+            // 如果是 空的，就直接回傳 空字串
+            if (string.IsNullOrEmpty(source))
+            {
+                return new List<string> { "" };
+            }
+
+            // 如果切割字串是 null 回傳整個
+            if (splitStr == null)
+            {
+                result.Add(source);
+                return result;
+            }
+
+            int len = tmpSource.Length;
+
+            // 如果 切割字串是 空字串，就每個字母來切
+            if (splitStr == string.Empty)
+            {
+                for (var i = 0; i < len; i++)
+                {
+                    string tmp = source.Substring(i, 1);
+                    result.Add(tmp);
+                }
+                return result;
+            }
+
+            // 其餘照著切
+            int splitStrLen = splitStr.Length;
+
+            // 判斷是否有進去
+            bool haveI = false;
+
+            // 位置
+            int pos = tmpSource.IndexOf(splitStr);
+
+            // 直到結束
+            while (pos >= 0)
+            {
+                haveI = true;
+                string tmp = string.Empty;
+
+                if (pos == 0)
+                {
+                    tmp = string.Empty;
+                }
+                else
+                {
+                    tmp = tmpSource.Substring(0, pos);
+                }
+
+                result.Add(tmp);
+
+                // 算出要延後幾位
+                int diff = pos + splitStrLen;
+
+                // 切割 (讓剩下的繼續跑)
+                tmpSource = tmpSource.Substring(diff);
+
+                // 重算位置
+                pos = tmpSource.IndexOf(splitStr);
+
+                // 如果 最後一次砍完剩下 空字串 => 要 push 進去
+                if (string.IsNullOrEmpty(tmpSource))
+                {
+                    result.Add(string.Empty);
+                }
+                else
+                {
+                    if (pos < 0)
+                    {
+                        result.Add(tmpSource);
+                    }
+                }
+            }
+
+            // 如果一開始就查不到 => 直接回傳 自己
+            if (haveI == false)
+            {
+                result.Add(source);
+            }
+
+            return result;
+        }
+    }
+}
