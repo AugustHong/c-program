47e7a864558862a6d682c4a023f17ed5bb4bede6
hitler11319 <hitler11319@gmail.com>
2025-01-03 19:53:12 +0800

    查已安裝程式清單

--- c#/查己安裝程式清單/查己安裝程式清單/Program.cs
+++ c#/查己安裝程式清單/查己安裝程式清單/Program.cs
@@ -12,6 +12,22 @@ using System.Threading.Tasks;
 
 namespace 查己安裝程式清單
 {
+
+    public class ProgramInfo
+    {
+        public string Name { get; set; }
+
+        public string Version { get; set; }
+
+        public string InstallDateStr { get; set; }
+
+        public string InstallLocation { get; set; }
+
+        public string Publish { get; set; }
+
+        public int? EstimatedSize { get; set; }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
@@ -39,6 +55,82 @@ namespace 查己安裝程式清單
             }
 
             Console.ReadLine();
+
+            // 延續上面的做法，做更進階 (這個比較優)，基本上跟上面的一樣
+            /*
+                備註：
+                (1) 有些抓到的還是很不準，沒有到我想要的像 控制台/程式集 那且的清楚
+                (2) 目前用 Get-StartApps 才真的抓到完全的。 像這裡的 我的 小烏龜 就一直沒出現，但我有安裝
+             */
+            List<ProgramInfo> totalInstallPrograms = GetTotalInstalledPrograms();
+            Console.WriteLine("名稱,發行商,安裝日期,版本號,檔案大小,安裝位置");
+            foreach (ProgramInfo info in totalInstallPrograms)
+            {
+                string EstimatedSize = info.EstimatedSize == null ? string.Empty : info.EstimatedSize.ToString();
+                Console.WriteLine($"{info.Name},{info.Publish},{info.InstallDateStr},{info.Version},{EstimatedSize} KB,{info.InstallLocation}");
+            }
+
+        }
+
+        /// <summary>
+        ///  取得 全部已安裝程式清單
+        /// </summary>
+        /// <returns></returns>
+        static List<ProgramInfo> GetTotalInstalledPrograms()
+        {
+            string registryKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+            List<ProgramInfo> localMachineList = GetInstalledPrograms(Registry.LocalMachine.OpenSubKey(registryKey));
+            List<ProgramInfo> currentUser = GetInstalledPrograms(Registry.CurrentUser.OpenSubKey(registryKey));
+
+            List<ProgramInfo> result = new List<ProgramInfo>();
+            result.AddRange(localMachineList);
+            result.AddRange(currentUser);
+
+            return result;
+        }
+
+        /// <summary>
+        /// 取得 安裝清單
+        /// </summary>
+        /// <param name="registryKey"></param>
+        /// <returns></returns>
+        static List<ProgramInfo> GetInstalledPrograms(RegistryKey registryKey)
+        {
+            List<ProgramInfo> result = new List<ProgramInfo>();
+            if (registryKey != null)
+            {
+                foreach (string subKey in registryKey.GetSubKeyNames())
+                {
+                    using (RegistryKey key = registryKey.OpenSubKey(subKey))
+                    {
+                        if (key != null)
+                        {
+                            string displayName = key.GetValue("DisplayName") as string;
+                            string displayVersion = key.GetValue("DisplayVersion") as string;
+                            string publisher = key.GetValue("Publisher") as string;
+                            string installDate = key.GetValue("InstallDate") as string;
+                            string installLocation = key.GetValue("InstallLocation") as string;
+                            int? estimatedSize = (int?)key.GetValue("EstimatedSize");
+
+                            if (!string.IsNullOrWhiteSpace(displayName))
+                            {
+                                result.Add(new ProgramInfo
+                                {
+                                    Name = displayName,
+                                    Version = displayVersion,
+                                    Publish = publisher,
+                                    InstallDateStr = installDate,
+                                    InstallLocation = installLocation,
+                                    EstimatedSize = estimatedSize
+                                });
+                            }
+                        }
+                    }
+                }
+
+            }
+
+            return result;
         }
     }
 }
