e65a6b7ccd3676343b4e4cf2e5ebb21a44e93810
hitler11319 <hitler11319@gmail.com>
2025-05-30 17:13:12 +0800

    Console程式的Read不要顯示文字

---
+++ c#/Console程式的Read不要顯示文字/Console程式的Read不要顯示文字/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console程式的Read不要顯示文字
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+                  string input1 = string.Empty;
+                  while (true)
+                  {
+                        ConsoleKeyInfo key = Console.ReadKey(true);
+                        if (key.Key == ConsoleKey.Enter)
+                        {
+                              Console.WriteLine();
+                              break;
+                        }
+
+                        input1 += key.KeyChar;
+                  }
+
+                  string input2 = string.Empty;
+                  while (true)
+                  {
+                        ConsoleKeyInfo key = Console.ReadKey(true);
+                        if (key.Key == ConsoleKey.Enter)
+                        {
+                              Console.WriteLine();
+                              break;
+                        }
+
+                        input2 += key.KeyChar;
+                  }
+
+                  Console.WriteLine($"你輸入的文字1 = {input1}");
+                  Console.WriteLine($"你輸入的文字2 = {input2}");
+                  Console.ReadLine();
+            }
+	}
+}
